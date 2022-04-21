f78a0c41cde4e916ea6e33f942fd1e7c589d2923
GfMRT <50573851+GfMRT@users.noreply.github.com>
2022-04-21 16:35:16 +0800

    Add files via upload

---
+++ Prob_generate.py
@@ -0,0 +1,381 @@
+import random
+
+class RESULTS:
+    def __init__(self):
+        self.CD6head = 0
+        self.CD5head = 0
+        self.CD4head_12 = 0
+        self.CD4head_9all = 0
+        self.CD4head_9 = 0
+        self.CD4head_6all = 0
+        self.CD4head = 0
+        self.meso_get_2row = 0
+        self.meso_get_3row = 0
+        self.top = 0
+        self.second_all = 0
+        self.second = 0
+        self.third = 0
+        self.third_2top = 0
+        self.third_1all = 0
+        self.third_2all = 0
+    
+    def add_equip(self, STR_percent, equip_i, CD, only_appear):
+        if equip_i == 0: # 帽子
+            if CD[1] >= 3:
+                self.CD6head = self.CD6head+1
+            elif CD[1] == 2:
+                if CD[0] >= 1:
+                    self.CD5head = self.CD5head+1
+                elif max(STR_percent)==12:
+                    self.CD4head_12 = self.CD4head_12+1
+                elif max(STR_percent)==9.2:
+                    self.CD4head_9all = self.CD4head_9all+1
+                elif max(STR_percent)==9:
+                    self.CD4head_9 = self.CD4head_9+1
+                elif max(STR_percent)==6.1:
+                    self.CD4head_6all = self.CD4head_6all+1
+                else:
+                    self.CD4head = self.CD4head+1
+            elif sum(STR_percent) == 36:
+                self.top = self.top+1
+            elif sum(STR_percent) == 33.2:
+                self.second_all = self.second_all+1
+            elif sum(STR_percent) == 33:
+                self.second = self.second+1
+            elif sum(STR_percent) == 30.1:
+                self.third_2top = self.third_2top+1
+            elif sum(STR_percent) == 30:
+                self.third = self.third+1
+            elif sum(STR_percent) == 30.2:
+                self.third_1all = self.third_1all+1
+            elif sum(STR_percent) == 30.4:
+                self.third_2all = self.third_2all+1
+            
+        elif equip_i == 3: # 手套
+            if sum(STR_percent) == 300:
+                self.top = self.top+1
+            elif sum(STR_percent) == 212:
+                self.second = self.second+1
+            elif sum(STR_percent) == 209.2:
+                self.second_all = self.second_all+1
+            elif sum(STR_percent) == 209:
+                self.third_2top = self.third_2top+1
+            elif sum(STR_percent) == 206.1:
+                self.third_1all = self.third_1all+1
+            elif sum(STR_percent) == 200:
+                self.third = self.third+1
+            
+        elif equip_i == 6: # 飾品
+            if sum(STR_percent) == 36:
+                self.top = self.top+1
+            elif sum(STR_percent) == 33.2:
+                self.second_all = self.second_all+1
+            elif sum(STR_percent) == 33:
+                self.second = self.second+1
+            elif CD[1] + CD[0] > 2: #次頂>3(楓+掉)
+                self.meso_get_3row = self.meso_get_3row+1 #only_appear 1技能/1被擊中後無敵時間增加, 2被擊中時有一定機率無視傷害/2掉寶, 2被擊時以一定機率一定時間內無敵
+            elif sum(STR_percent) == 30.1:
+                self.third_2top = self.third_2top+1
+            elif sum(STR_percent) == 30:
+                self.third = self.third+1
+            elif sum(STR_percent) == 30.2:
+                self.third_1all = self.third_1all+1
+            elif sum(STR_percent) == 30.4:
+                self.third_2all = self.third_2all+1
+            elif CD[1] + CD[0] == 2:
+                self.meso_get_2row = self.meso_get_2row+1
+                
+        else:
+            if sum(STR_percent) == 36:
+                self.top = self.top+1
+            elif sum(STR_percent) == 33.2:
+                self.second_all = self.second_all+1
+            elif sum(STR_percent) == 33:
+                self.second = self.second+1
+            elif sum(STR_percent) == 30.1:
+                self.third_2top = self.third_2top+1
+            elif sum(STR_percent) == 30:
+                self.third = self.third+1
+            elif sum(STR_percent) == 30.2:
+                self.third_1all = self.third_1all+1
+            elif sum(STR_percent) == 30.4:
+                self.third_2all = self.third_2all+1
+                
+    def output(self, equip_i):
+        equip = ['帽', '衣,套服', '褲', '手', '鞋', '披風,腰帶,肩膀', '飾']
+        print('\n', equip[equip_i])
+        
+        if equip_i == 0: #帽子
+            print("    6CD: ", self.CD6head)
+            print("    5CD: ", self.CD5head)
+            print("    4CD: ", self.CD4head_12 + self.CD4head_9all + self.CD4head_9 + self.CD4head_6all + self.CD4head)
+            print("        4CD13屬: ", self.CD4head_12)
+            print("        4CD10全: ", self.CD4head_9all)
+            print("        4CD10屬: ", self.CD4head_9)
+            print("        4CD7全: ", self.CD4head_6all)
+            print("        4CD爛: ", self.CD4head)
+            print("    頂: ", self.top)
+            print("    次頂: ", self.second_all + self.second)
+            print("        含全: ", self.second_all)
+            print("    次次: ", self.third_2top + self.third + self.third_1all + self.third_2all)
+            print("        含6全: ", self.third_2top)
+            print("        含9全: ", self.third_1all)
+            print("        含18全: ", self.third_2all)
+        elif equip_i == 3: #手套
+            print("    三爆: ", self.top)
+            print("    雙爆: ", self.second + self.second_all + self.third_2top + self.third_1all + self.third)
+            print("        雙爆13: ", self.second)
+            print("        雙爆10全: ", self.second_all)
+            print("        雙爆10: ", self.third_2top)
+            print("        雙爆7全:  ", self.third_1all)
+            print("        雙爆爛: ", self.third)
+        else:
+            print("    頂: ", self.top)
+            print("    次頂: ", self.second_all + self.second)
+            print("        含全: ", self.second_all)
+            print("    次次: ", self.third_2top + self.third + self.third_1all + self.third_2all)
+            print("        含6全: ", self.third_2top)
+            print("        含9全: ", self.third_1all)
+            print("        含18全: ", self.third_2all)
+        
+        if equip_i == 6: #飾品
+            print("    兩排楓掉: ", self.meso_get_2row)
+            print("    三排楓掉: ", self.meso_get_3row)
+
+class Prob:
+    def __init__(self, cube): # 0/1/2 = 閃炫、鏡射、對等
+        if(cube == 0):
+            # 閃炫
+            self.ingore_damage_A = [0.127, 0.1096, 0.1588, 0.1194, 0.127, 0.1356, 0] #2被擊中時有一定機率無視傷害
+            self.equip_A_STR = [0.0794, 0.0685, 0.0794, 0.0746, 0.0794, 0.0847, 0.098]
+            self.equip_A_all = [0.0635, 0.0548, 0.0635, 0.0597, 0.0635, 0.0678, 0.0784]
+            self.ingore_damage_S = [0.1510, 0.1568, 0.1778, 0.1538, 0.1666, 0.1778, 0]
+            self.equip_S_STR = [0.0755, 0.0784, 0.0889, 0.0769, 0.0833, 0.0889, 0.0784]
+            self.equip_S_all = [0.0566, 0.0588, 0.0667, 0.0577, 0.0625, 0.0667, 0.0588]
+            self.equip_critical = 0.0769
+            self.equip_CD1 = 0.0566
+            self.equip_CD2 = 0.0377
+            self.skill_head_door_A = 0.0635     # 1帽子:實用的時空門
+            self.invincible_time_A = 0.0548     # 1衣服:被擊中後無敵時間增加
+            self.invincible_prob_A = 0.0548     # 2衣服:被擊時以一定機率一定時間內無敵
+            self.skill_pant_fire_A = 0.0635     # 1褲子:實用的神聖之火
+            self.skill_glove_eye_A = 0.0597     # 1手套:實用的會心之眼
+            self.skill_shoe_speed_A = 0.0635    # 1鞋子:實用的速度激發
+            self.skill_head_bless_S = 0.0566    # 1帽子:實用的祝福
+            self.invincible_time_S = 0.0588     # 1衣服:被擊中後無敵時間增加
+            self.invincible_prob_S = 0.0588     # 1衣服:被擊時以一定機率一定時間內無敵
+            self.skill_glove_speed_S = 0.0577   # 1手套:實用的最終極速
+            self.skill_shoe_order_S = 0.0625    # 1鞋子:實用的戰鬥命令
+            self.item_get_S = 0.0588            # 1飾品:道具掉落率
+            self.meso_get = 0.0588
+        elif(cube == 1):
+            # 鏡射
+            self.ingore_damage_A = [0.1778, 0.1454, 0.2222, 0.1632, 0.1778, 0.1952, 0]
+            self.equip_A_STR = [0.0444, 0.0364, 0.0444, 0.0408, 0.0444, 0.0488, 0.0606]
+            self.equip_A_all = [0.0444, 0.0364, 0.0444, 0.0408, 0.0444, 0.0488, 0.0606]
+            self.ingore_damage_S = [0.1904, 0.2, 0.2352, 0.1952, 0.2162, 0.2352, 0]
+            self.equip_S_STR = [0.0476, 0.05, 0.0588, 0.0488, 0.0541, 0.0588, 0.0784]
+            self.equip_S_all = [0.0476, 0.05, 0.0588, 0.0488, 0.0541, 0.0588, 0.0588]
+            self.equip_critical = 0.0976
+            self.equip_CD1 = 0.0714
+            self.equip_CD2 = 0.0476
+            self.skill_head_door_A = 0.0889
+            self.invincible_time_A = 0.0727
+            self.invincible_prob_A = 0.0727
+            self.skill_pant_fire_A = 0.0889
+            self.skill_glove_eye_A = 0.0816
+            self.skill_shoe_speed_A = 0.0889
+            self.skill_head_bless_S = 0.0714
+            self.invincible_time_S = 0.075
+            self.invincible_prob_S = 0.075
+            self.skill_glove_speed_S = 0.0732
+            self.skill_shoe_order_S = 0.0811
+            self.item_get_S = 0.075
+            self.meso_get = 0.075
+        elif(cube == 2):
+            # 對等
+            self.ingore_damage_S = [0.1510, 0.1568, 0.1778, 0.1538, 0.1666, 0.1778, 0]
+            self.equip_S_STR = [0.0755, 0.0784, 0.0889, 0.0769, 0.0833, 0.0889, 0.0784]
+            self.equip_S_all = [0.0566, 0.0588, 0.0667, 0.0577, 0.0625, 0.0667, 0.0588]
+            self.equip_critical = 0.0769
+            self.equip_CD1 = 0.0566
+            self.equip_CD2 = 0.0377
+            self.skill_head_bless_S = 0.0566
+            self.invincible_time_S = 0.0588
+            self.invincible_prob_S = 0.0588
+            self.skill_glove_speed_S = 0.0577
+            self.skill_shoe_order_S = 0.0625
+            self.item_get_S = 0.0588
+            self.meso_get = 0.0588
+    
+    def getS(self, state_percent, kind, CD, only_appear): 
+        #only_appear 1技能/1被擊中後無敵時間增加, 2被擊中時有一定機率無視傷害/2掉寶, 2被擊時以一定機率一定時間內無敵
+        line = random.random()
+        cummulate = 0
+        
+        #決定機率倍率
+        multiplier = 1
+        if only_appear[0]>=1:
+            if kind == 0:
+                multiplier = multiplier/(1-self.skill_head_bless_S)
+            elif kind == 1:
+                multiplier = multiplier/(1-self.invincible_time_S)
+            elif kind == 3:
+                multiplier = multiplier/(1-self.skill_glove_speed_S)
+            elif kind == 4:
+                multiplier = multiplier/(1-self.skill_shoe_order_S)
+        if only_appear[1]>=2:
+            if kind == 6:
+                multiplier = multiplier/(1-self.item_get_S)
+            else:
+                multiplier = multiplier/(1-self.ingore_damage_S[kind])
+        if only_appear[2]>=2:
+            multiplier = multiplier/(1-self.invincible_prob_S)
+        
+        #開始抽
+        cummulate = cummulate + self.equip_S_STR[kind] * multiplier
+        if (line < cummulate):
+            if min(state_percent) <12:
+                state_percent[state_percent.index(min(state_percent))] = 12
+            return
+        else:
+            cummulate = cummulate + self.equip_S_all[kind] * multiplier
+            
+        if (line < cummulate):
+            if min(state_percent) <9.2:
+                state_percent[state_percent.index(min(state_percent))] = 9.2
+            return
+        elif kind == 0:
+            cummulate = cummulate + self.equip_CD1 * multiplier
+        elif kind == 3:
+            cummulate = cummulate + self.equip_critical * multiplier
+        elif kind == 6:
+            cummulate = cummulate + self.meso_get * multiplier
+            
+        if (line < cummulate):
+            if kind == 0:
+                CD[0] = CD[0]+1
+                return
+            elif kind == 3:
+                if min(state_percent) <100:
+                    state_percent[state_percent.index(min(state_percent))] = 100
+                return
+            elif kind == 6:
+                CD[0] = CD[0]+1
+                return    
+        elif kind == 0:
+            cummulate = cummulate + self.equip_CD2 * multiplier
+            
+        if (line < cummulate):
+            if kind == 0:
+                CD[1] = CD[1]+1
+                return
+                
+        #沒有洗到排數限制的抽完了  
+        #第一種限制        
+        if only_appear[0]<1:
+            if kind == 0:
+                cummulate = cummulate + self.skill_head_bless_S * multiplier
+            elif kind == 1:
+                cummulate = cummulate + self.invincible_time_S * multiplier
+            elif kind == 3:
+                cummulate = cummulate + self.skill_glove_speed_S * multiplier
+            elif kind == 4:
+                cummulate = cummulate + self.skill_shoe_order_S * multiplier
+                
+        if (line < cummulate):
+            only_appear[0] = only_appear[0]+1
+            return
+        
+        #第二種限制
+        if only_appear[1]<2:
+            if kind == 6: #CD[0]用於飾品 = 楓幣
+                cummulate = cummulate + self.item_get_S * multiplier
+            else:
+                cummulate = cummulate + self.ingore_damage_S[kind] * multiplier
+                
+        if (line < cummulate):
+            only_appear[1] = only_appear[1]+1
+            if kind == 6:
+                CD[1] = CD[1]+1
+            return
+        
+        #第三種限制
+        if only_appear[2]<2:
+            if kind == 1:
+                cummulate = cummulate + self.invincible_prob_S * multiplier
+                
+        if (line < cummulate):
+            only_appear[2] = only_appear[2]+1
+        
+        
+    def getA(self, state_percent, kind, only_appear): 
+        #only_appear 1技能/1被擊中後無敵時間增加, 2被擊中時有一定機率無視傷害/2掉寶, 2被擊時以一定機率一定時間內無敵
+        line = random.random()
+        cummulate = 0
+        
+        #決定機率倍率
+        multiplier = 1
+        if only_appear[0]>=1:
+            if kind == 0:
+                multiplier = multiplier/(1-self.skill_head_door_A)
+            elif kind == 1:
+                multiplier = multiplier/(1-self.invincible_time_A)
+            elif kind == 2:
+                multiplier = multiplier/(1-self.skill_pant_fire_A)
+            elif kind == 3:
+                multiplier = multiplier/(1-self.skill_glove_eye_A)
+            elif kind == 4:
+                multiplier = multiplier/(1-self.skill_shoe_speed_A)
+        if only_appear[1]>=2:
+            multiplier = multiplier/(1-self.ingore_damage_A[kind])
+        if only_appear[2]>=2:
+            multiplier = multiplier/(1-self.invincible_prob_A)
+        
+        #開始抽
+        cummulate = cummulate + self.equip_A_STR[kind] * multiplier
+        if (line < cummulate):
+            if min(state_percent) <9:
+                state_percent[state_percent.index(min(state_percent))] = 9
+            return
+        else:
+            cummulate = cummulate + self.equip_A_all[kind] * multiplier
+            
+        if (line < cummulate):
+            if min(state_percent) <6.1:
+                state_percent[state_percent.index(min(state_percent))] = 6.1
+            return
+                
+        #沒有洗到排數限制的抽完了  
+        #第一種限制        
+        if only_appear[0]<1:
+            if kind == 0:
+                cummulate = cummulate + self.skill_head_door_A * multiplier
+            elif kind == 1:
+                cummulate = cummulate + self.invincible_time_A * multiplier
+            elif kind == 2:
+                cummulate = cummulate + self.skill_pant_fire_A * multiplier
+            elif kind == 3:
+                cummulate = cummulate + self.skill_glove_eye_A * multiplier
+            elif kind == 4:
+                cummulate = cummulate + self.skill_shoe_speed_A * multiplier
+                
+        if (line < cummulate):
+            only_appear[0] = only_appear[0]+1
+            return
+        
+        #第二種限制
+        if only_appear[1]<2:
+            cummulate = cummulate + self.ingore_damage_A[kind] * multiplier
+                
+        if (line < cummulate):
+            only_appear[1] = only_appear[1]+1
+            return
+        
+        #第三種限制
+        if only_appear[2]<2:
+            if kind == 1:
+                cummulate = cummulate + self.invincible_prob_A * multiplier
+                
+        if (line < cummulate):
+            only_appear[2] = only_appear[2]+1
--- 洗裝備對等.py
+++ 洗裝備對等.py
@@ -1,185 +1,30 @@
 import random
 import os
-
-# 鏡射
-equip = ['帽', '衣,套服', '褲', '手', '鞋', '披風,腰帶,肩膀', '飾']
-equip_S_STR = [0.0755, 0.0784, 0.0889, 0.0769, 0.0833, 0.0889, 0.0784]
-equip_S_all = [0.0566, 0.0588, 0.0667, 0.0577, 0.0625, 0.0667, 0.0588]
-equip_critical = 0.0769
-equip_CD1 = 0.0566
-equip_CD2 = 0.0377
-skill_head_bless_S = 0.0566
-invincible_clothes_S = 0.0588
-skill_glove_speed_S = 0.0577
-skill_shoe_order_S = 0.0625
-item_get_S = 0.0588
-
-# 累進機率
-for i in range(0,7):
-    equip_S_all[i] = equip_S_all[i] + equip_S_STR[i]
-equip_critical = equip_critical+equip_S_all[3]
-equip_CD1 = equip_CD1 + equip_S_all[0]
-equip_CD2 = equip_CD2 + equip_CD1
-
-def getS(state_percent, kind, CD, only_appear):
-    line = random.random()
-    
-    if kind == 0: #帽子的實用
-        if only_appear[0]==1:
-            line = line * (1-skill_head_bless_S)
-        elif line>(1-skill_head_bless_S):
-            only_appear[0] = only_appear[0]+1
-    elif kind == 1: #衣服的無敵
-        if only_appear[0]==1:
-            line = line * (1-invincible_clothes_S)
-        elif line>(1-invincible_clothes_S):
-            only_appear[0] = only_appear[0]+1
-    elif kind == 3: #手套的實用
-        if only_appear[0]==1:
-            line = line * (1-skill_glove_speed_S)
-        elif line>(1-skill_glove_speed_S):
-            only_appear[0] = only_appear[0]+1
-    elif kind == 4: #鞋子的實用
-        if only_appear[0]==1:
-            line = line * (1-skill_shoe_order_S)
-        elif line>(1-skill_shoe_order_S):
-            only_appear[0] = only_appear[0]+1
-    elif kind == 6: #掉寶
-        if only_appear[0]==2:
-            line = line * (1-item_get_S)
-        elif line>(1-item_get_S):
-            only_appear[0] = only_appear[0]+1
-    
-    if (line < equip_S_STR[kind]):
-        if min(state_percent) <12:
-            state_percent[state_percent.index(min(state_percent))] = 12
-    elif (line < equip_S_all[kind]):
-        if min(state_percent) <9.2:
-            state_percent[state_percent.index(min(state_percent))] = 9.2
-    elif kind == 3:
-        if (line < equip_critical):
-            if min(state_percent) <100:
-                state_percent[state_percent.index(min(state_percent))] = 100
-    elif kind == 0:
-        if (line < equip_CD1):
-            CD[0] = CD[0]+1
-        elif (line < equip_CD2):
-            CD[1] = CD[1]+1
-        
-CD6head = 0
-CD5head = 0
-CD4head_12 = 0
-CD4head_9all = 0
-CD4head = 0
-only_appear = [0]
+import Prob_generate
 
 print("洗對等")
+cube_kind = 2
+Prob = Prob_generate.Prob(cube_kind)
+
 for equip_i in range(0,7):
-    top = 0
-    second_all = 0
-    second = 0
-    third = 0
-    third_2top = 0
-    third_1all = 0
-    third_2all = 0
-    
-    for exp in range(0,int(1e7)):
+    results = Prob_generate.RESULTS()
+    for exp in range(0,int(1e8)):
         CD = [0,0]
-        only_appear[0] = 0
+        only_appear = [0,0,0]
         STR_percent = [0,0,0]
         # 第1排
-        getS(STR_percent, equip_i, CD, only_appear)
+        Prob.getS(STR_percent, equip_i, CD, only_appear)
         
         # 第2排
-        getS(STR_percent, equip_i, CD, only_appear)
+        Prob.getS(STR_percent, equip_i, CD, only_appear)
             
         # 第3排
-        getS(STR_percent, equip_i, CD, only_appear)
+        Prob.getS(STR_percent, equip_i, CD, only_appear)
         
         # 確認要的裝備
-        if equip_i == 0:
-            if CD[1] >= 3:
-                CD6head = CD6head+1
-            elif CD[1] == 2:
-                if CD[0] >= 1:
-                    CD5head = CD5head+1
-                elif max(STR_percent)==12:
-                    CD4head_12 = CD4head_12+1
-                elif max(STR_percent)==9.2:
-                    CD4head_9all = CD4head_9all+1
-                else:
-                    CD4head = CD4head+1
-            elif sum(STR_percent) == 36:
-                top = top+1
-            elif sum(STR_percent) == 33.2:
-                second_all = second_all+1
-            elif sum(STR_percent) == 33:
-                second = second+1
-            elif sum(STR_percent) == 30.1:
-                third_2top = third_2top+1
-            elif sum(STR_percent) == 30:
-                third = third+1
-            elif sum(STR_percent) == 30.2:
-                third_1all = third_1all+1
-            elif sum(STR_percent) == 30.4:
-                third_2all = third_2all+1
-            
-        elif equip_i == 3:
-            if sum(STR_percent) == 300:
-                top = top+1
-            elif sum(STR_percent) == 212:
-                second = second+1
-            elif sum(STR_percent) == 209.2:
-                second_all = second_all+1
-            elif sum(STR_percent) == 209:
-                third_2top = third_2top+1
-            elif sum(STR_percent) == 206.1:
-                third_1all = third_1all+1
-            elif sum(STR_percent) == 200:
-                third = third+1
-                
-        else:
-            if sum(STR_percent) == 36:
-                top = top+1
-            elif sum(STR_percent) == 33.2:
-                second_all = second_all+1
-            elif sum(STR_percent) == 33:
-                second = second+1
-            elif sum(STR_percent) == 30.1:
-                third_2top = third_2top+1
-            elif sum(STR_percent) == 30:
-                third = third+1
-            elif sum(STR_percent) == 30.2:
-                third_1all = third_1all+1
-            elif sum(STR_percent) == 30.4:
-                third_2all = third_2all+1
-    
-    print('\n', equip[equip_i])
-    
-    if equip_i == 0: #帽子
-        print("    6CD: ", CD6head)
-        print("    5CD: ", CD5head)
-        print("    4CD: ", CD4head_12+CD4head_9all+CD4head)
-        print("        4CD13屬: ", CD4head_12)
-        print("        4CD10全: ", CD4head_9all)
-        print("\n\n        4CD爛: ", CD4head)
-        print("    頂: ", top)
-        print("    次頂: ", second_all+second)
-        print("        含全: ", second_all)
-        print("    次次: ", third_2top + third + third_1all + third_2all)
-        print("\n\n        含18全: ", third_2all)
-    elif equip_i == 3: #手套
-        print("    三爆: ", top)
-        print("    雙爆: ", second + second_all + third_2top + third_1all + third)
-        print("        雙爆13: ", second)
-        print("        雙爆10全: ", second_all)
-        print("\n\n        雙爆爛: ", third)
-    else:
-        print("    頂: ", top)
-        print("    次頂: ", second_all+second)
-        print("        含全: ", second_all)
-        print("    次次: ", third_2top + third + third_1all + third_2all)
-        print("\n\n        含18全: ", third_2all)
+        results.add_equip(STR_percent, equip_i, CD, only_appear)
+        
+    results.output(equip_i)  
     
 os.system("pause")
             
